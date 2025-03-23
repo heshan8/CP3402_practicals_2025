e0f14f9c73ae67eed9ee651178fec8b3059948e4
heshan8 <heshan988@gmail.com>
2025-03-23 19:18:38 +1000

    Create header with navigation

---
+++ week_7/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>Welcome to my site</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <h1>Welcome to My Favorite Things</h1>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+    </nav>
+</header>
+<main>
