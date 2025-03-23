c60110dae13793f8160940bc110b42d95e7bb5d7
heshan8 <heshan988@gmail.com>
2025-03-23 19:19:50 +1000

    Create footer

---
+++ week_7/footer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <title>Welcome to my site</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> My Favorite Things</p>
+    </footer>
+<main>
