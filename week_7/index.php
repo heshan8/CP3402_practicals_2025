bf38fdd0d67762b991c3f2053d534130d4c7975f
heshan8 <heshan988@gmail.com>
2025-03-23 19:50:55 +1000

    Add foreach loop using an array

--- week_7/index.php
+++ week_7/index.php
@@ -1,10 +1,19 @@
 <?php include 'header.php'; ?>
 
 <?php
+// Echo - echoing different kinds of HTML
 // using Echo to print HTML
 echo "<h1>Movies</h1>";
-echo "<p>Here's a list of some of my favorite movies:</p>";
+echo "<p>Here's a list of some of my favorite tv shows:</p>";
 
 
+//Repetition - for, while, foreach (use an array)
+//foreach loop using an array
+echo "<ul>";
 
+$favoriteShows = ["True Detective", "Fargo", "The X-Files", "Severance", "Lost"];
+foreach ($favoriteShows as $show) {
+    echo "<li>$show</li>";    }
+
+echo "<ul>";
 
